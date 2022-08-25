d1b3b012f25a013f4cdb8d78d12fc1b16ba31353
Jovo Nikolic <jnikolic.win-b2019@fh-salzburg.ac.at>
2022-08-25 15:30:08 +0200

    Enable login with mandants table

--- config/auth.php
+++ config/auth.php
@@ -15,7 +15,7 @@ return [
 
     'defaults' => [
         'guard' => 'web',
-        'passwords' => 'users',
+        'passwords' => 'mandants',
     ],
 
     /*
@@ -38,7 +38,7 @@ return [
     'guards' => [
         'web' => [
             'driver' => 'session',
-            'provider' => 'users',
+            'provider' => 'mandants',
         ],
     ],
 
@@ -60,9 +60,9 @@ return [
     */
 
     'providers' => [
-        'users' => [
+        'mandants' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'model' => App\Models\Mandant::class,
         ],
 
         // 'users' => [
@@ -87,8 +87,8 @@ return [
     */
 
     'passwords' => [
-        'users' => [
-            'provider' => 'users',
+        'mandants' => [
+            'provider' => 'mandants',
             'table' => 'password_resets',
             'expire' => 60,
             'throttle' => 60,
