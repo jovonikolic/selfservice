0582dcafadfd09d31082a4ac076656e7dfd56555
Jovo Nikolic <jnikolic.win-b2019@fh-salzburg.ac.at>
2022-11-17 11:46:49 +0100

    Prepare the routes for the calls from the frontend to the backend

--- routes/web.php
+++ routes/web.php
@@ -52,3 +52,6 @@ Route::get('/csvExport/{type}', [CsvController::class, 'getCSV']);
 
 Route::get('/chargingProcessAnalytics', [ChargingProcessController::class, 'analyzeChargingTimeline']);
 Route::get('/getDataForChargingProcessAnalytics', [ChargingProcessController::class, 'getDataForChargingProcessAnalytics']);
+Route::get('/getWeeklyChargingProcessData', [ChargingProcessController::class, 'getWeeklyChargingProcessData']);
+Route::get('/getMonthlyChargingProcessData', [ChargingProcessController::class, 'getMonthlyChargingProcessData']);
+Route::get('/getLatestChargingProcess', [ChargingProcessController::class, 'getLatestChargingProcess']);
